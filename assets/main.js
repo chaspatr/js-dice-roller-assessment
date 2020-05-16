35f7f0a151181168905af5e4a8725f3646a2d011
Chase Patrick <chasepatrick117@gmail.com>
2020-05-15 22:47:24 -0400

    initialized after first day of work

---
+++ assets/main.js
@@ -0,0 +1,47 @@
+//Declare variables from input and results
+
+const rollButton = document.querySelector(".roll-button");
+const resetButton = document.querySelector(".reset-button");
+const showRollsButton = document.querySelector(".show-rolls-button");
+const dieRolls = [];
+let totalRoll = document.querySelector(".total-value");
+let numberOfRolls = document.querySelector(".number-of-rolls");
+let index = 1;
+let currentRoll = 0;
+let sumofRolls = 0;
+
+//event listener fo the roll button that loops a number of times that equals to the number from the 'number-of-rolls' input element
+
+rollButton.addEventListener("click", function () {
+  numberOfRolls = document.querySelector(".number-of-rolls");
+  index = 1;
+  while (index <= numberOfRolls.value) {
+    currentRoll = Math.floor(Math.random() * 6 + 1);
+    console.log("Roll " + index + " = " + currentRoll);
+    //also add the value of each roll to get the total value of the rolls and display it in the 'total-value' class
+    sumofRolls = sumofRolls + currentRoll;
+    index++;
+    //pushing each roll's value to the array 'die rolls'
+    dieRolls.push(currentRoll);
+  }
+  console.log(dieRolls);
+  console.log(sumofRolls);
+  totalRoll.innerHTML = sumofRolls;
+});
+
+//event listener for the reset button to reset all relevent values back to its original state
+resetButton.addEventListener("click", function () {
+  index = 1;
+  currentRoll = 0;
+  sumofRolls = 0;
+  numberOfRolls.value = 0;
+  totalRoll.innerHTML = 0;
+});
+
+//event listener for the show rolls button to display the value of each roll from the array that is storing the data
+showRollsButton.addEventListener("click", function () {
+  let counter = 1;
+  while (counter <= numberOfRolls) {
+    counter++;
+  }
+});
